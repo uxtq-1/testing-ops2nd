d928b09ffab5a989f5af1bbb7fae42addcda162c
uxtq-1 <gabrieloor.cv@gmail.com>
2025-02-24 21:59:18 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("DOM fully loaded and parsed.");
 
   // ============================
   // 1) Theme Toggle
